718560cc69d862263ee4954e5f0a64555e66448a
tianma <tianma@maike51.com>
2018-09-10 00:13:30 +0800

    第74讲-Spark SQL：使用反射方式将RDD转换为DataFrame

---
+++ sparkJava/src/main/java/com/beifeng/sql/Student.java
@@ -0,0 +1,46 @@
+package com.beifeng.sql;
+
+import java.io.Serializable;
+
+/**
+ * 学生JavaBean
+ * time: 2018-09-09 21:24
+ */
+public class Student implements Serializable {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
